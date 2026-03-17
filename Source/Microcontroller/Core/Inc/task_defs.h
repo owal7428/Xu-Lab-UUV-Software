e223f675b3618ca59019fd817ea53aca8d8989c3
Leon Steinbach <lest9540@colorado.edu>
2026-03-17 17:14:37 -0500

    Add task declerations and initializations

---
+++ Source/Microcontroller/Core/Inc/task_defs.h
@@ -0,0 +1,159 @@
+/*
+ * task_defs.h
+ *
+ *  Created on: 17 Mar 2026
+ *      Author: leons
+ */
+
+#include "main.h"
+#include "cmsis_os.h"
+#include "usb_device.h"
+#include "freertos.h"
+#include "queue.h"
+#include <math.h>
+#include <stdbool.h>
+
+#ifndef CORE_INC_TASK_DEFS_H_
+#define CORE_INC_TASK_DEFS_H_
+
+#define INITPAUSE 7000 // 7.0 s
+#define PACKETSIZE 128 // 128 bytes
+
+
+
+/* external definitions of connection ports */
+extern ADC_HandleTypeDef hadc1;
+extern I2C_HandleTypeDef hi2c1;
+extern SPI_HandleTypeDef hspi1;
+extern SPI_HandleTypeDef hspi2;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern UART_HandleTypeDef huart4;
+extern UART_HandleTypeDef huart5;
+
+
+
+/*
+ *  Struct defenitions for communicating commands from the Raspberry Pi to the tasks
+ */
+typedef struct
+{
+	float amplitude;
+	uint16_t speed;
+	int16_t horz_offset;
+	int16_t vert_offset;
+} ServoCmd_t;
+
+typedef struct
+{
+	int8_t thrust;
+} ThrusterCmd_t;
+
+
+
+/*
+ *  Struct defenitions for communicating task data to the Raspberry Pi communication task
+ */
+typedef struct
+{
+	float acc_x;
+	float acc_y;
+	float acc_z;
+	float gyro_x;
+	float gyro_y;
+	float gyro_z;
+}IMUCom_t;
+
+typedef struct
+{
+	float head_x;
+	float head_y;
+	float head_z;
+}MagCom_t;
+
+typedef struct
+{
+	float pressure;
+	float water_temp;
+}Bar30Com_t;
+
+typedef struct
+{
+	float humidity;
+	float air_temp;
+}HumidCom_t;
+
+typedef struct
+{
+	float board_temp;
+}BoardCom_t;
+
+
+
+/*
+ *  Struct defenition for assembling communication packet out to the Raspberry Pi
+ */
+typedef struct
+{
+	IMUCom_t IMUCom;
+	MagCom_t MagCom;
+	Bar30Com_t Bar30Com;
+	HumidCom_t HumidCom;
+	BoardCom_t BoardCom;
+} OutPacket_t;
+
+
+
+/*
+ *  Struct defenitions for interpreting communication packets from the Raspberry Pi
+ */
+typedef struct
+{
+	ServoCmd_t ServoCmd[8];
+	ThrusterCmd_t ThrusterCmd;
+} InPacket_t;
+
+
+
+/* Helper struct for communicating to servos which channel and tim they are for initialization and ID */
+typedef struct
+{
+    TIM_HandleTypeDef *tim;
+    uint32_t channel;
+} ServoParams_t;
+
+
+
+/* Function definitions for tasks and helper functions */
+void LED_Task(void* argument);
+void PiCom_Task(void *argument);
+
+uint16_t angle_to_pulse(int16_t angle);
+void Servo_Init(TIM_HandleTypeDef* tim, uint32_t channel);
+void Servo_Task(void* argument);
+
+uint16_t percent_to_pulse(int8_t percent);
+void Thruster_Init(void);
+void Thruster_Task(void* arugment);
+
+void SPI_WriteReg(uint8_t line, uint8_t chip, uint8_t reg, uint8_t data);
+uint8_t SPI_ReadReg(uint8_t line, uint8_t chip, uint8_t reg);
+void SPI_Read(uint8_t line, uint8_t chip, uint8_t reg, uint8_t* buffer, uint8_t length);
+
+bool IMU_Init(void);
+void IMU_Task(void *argument);
+
+bool Mag_Init(void);
+void Mag_Task(void *argument);
+
+void Bar30_Init(void);
+void Bar30_Task(void* argument);
+
+bool Humid_Init(void);
+void Humid_Task(void *argument);
+
+bool Board_Init(void);
+void Board_Task(void *argument);
+
+#endif /* CORE_INC_TASK_DEFS_H_ */
---
+++ Source/Microcontroller/Core/Src/task_defs.c
@@ -0,0 +1,251 @@
+/*
+  ******************************************************************************
+  * @file           : task_defs.c
+  * @brief          : Definitions for reading and writing to peripherals
+  ******************************************************************************
+  */
+
+#include "task_defs.h"
+
+
+
+/* Definition of handles for all the tasks' communication queues */
+QueueHandle_t IMUQueue;
+QueueHandle_t MagQueue;
+QueueHandle_t Bar30Queue;
+QueueHandle_t HumidQueue;
+QueueHandle_t BoardQueue;
+QueueHandle_t ThrusterQueue;
+QueueHandle_t ServoQueue[8];
+
+/* Mutex lock for making sure that SPI1 communication doesn't collide */
+osMutexId_t spiMutex;
+
+/* Array to store parameters that ID each servo task to its corresponding servo */
+ServoParams_t servoParams[2][4];
+
+
+
+/*
+ *  Test Task for checking flashing and power
+ */
+void LED_Task(void* argument)
+{
+	for (;;)
+	{
+		HAL_GPIO_TogglePin(GPIOC, LED_OUT1_Pin);
+		osDelay(50);
+		HAL_GPIO_TogglePin(GPIOC, LED_OUT1_Pin);
+		osDelay(50);
+		HAL_GPIO_TogglePin(LED_OUT3_GPIO_Port, LED_OUT3_Pin);
+		osDelay(500);
+	}
+}
+
+/*
+ *  Handles communication back and forth to and from the raspberry pi by assembling and deciphering packets
+ */
+void PiCom_Task(void * argument)
+{
+	uint8_t tx[PACKETSIZE];
+	uint8_t rx[PACKETSIZE];
+	OutPacket_t out;
+	InPacket_t in;
+	ThrusterCmd_t thrusterCommand;
+	ServoCmd_t servoCommand[8];
+
+	// TODO: Establish communications check with Raspberry Pi
+	// TODO: Check which sensors are online for queue receiving
+	// TODO: Report to Raspberry Pi which sensors are offline
+
+	for (;;)
+	{
+		/* clean all communications structures to avoid sending old data */
+		memset(tx, 0, PACKETSIZE);
+		memset(rx, 0, PACKETSIZE);
+		memset(&out, 0, sizeof(OutPacket_t));
+		memset(&in, 0, sizeof(InPacket_t));
+		memset(&thrusterCommand, 0, sizeof(ThrusterCmd_t));
+		for (int i=0; i<8; i++) memset(&servoCommand[i], 0, sizeof(ServoCmd_t));
+
+		/* collect new data from all sensor queues into the outgoing packet */
+		// TODO: Adjust queue timeouts in case of queues underflowing
+		xQueueReceive(IMUQueue, &out.IMUCom, 1);
+		xQueueReceive(MagQueue, &out.MagCom, 1);
+		xQueueReceive(Bar30Queue, &out.Bar30Com, 1);
+		xQueueReceive(HumidQueue, &out.HumidCom, 1);
+		xQueueReceive(BoardQueue, &out.BoardCom, 1);
+
+		/* serialize packet struct into bytes */
+		memcpy(tx, &out, sizeof(OutPacket_t));
+
+		/* perform transfer (using NSS so no need to manually pull any chips low or high) */
+		HAL_SPI_TransmitReceive(&hspi2, tx, rx, PACKETSIZE, HAL_MAX_DELAY);
+
+		/* de-serialize byte packet into struct */
+		memcpy(rx, &in, sizeof(InPacket_t));
+
+		/* distribute commands */
+		// TODO: Adjust queue timeouts in case of queue overflowing
+		// TODO: Avoid sending duplicate commands to thruster or servos
+		xQueueSend(ThrusterQueue, &in.ThrusterCmd, 1);
+		for (int i=0; i<8; i++) xQueueSend(ServoQueue[i], &in.ServoCmd[i], 1);
+	}
+}
+
+
+
+void SPI_WriteReg(uint8_t line, uint8_t chip, uint8_t reg, uint8_t data)
+{
+	//
+}
+
+uint8_t SPI_ReadReg(uint8_t line, uint8_t chip, uint8_t reg)
+{
+	//
+}
+
+void SPI_Read(uint8_t line, uint8_t chip, uint8_t reg, uint8_t* buffer, uint8_t length)
+{
+	//
+}
+
+
+
+bool IMU_Init(void)
+{
+	//
+}
+
+void IMU_Task(void *argument)
+{
+	IMUCom_t output = {0};
+
+	for (;;)
+	{
+		xQueueSend(IMUQueue, &output, HAL_MAX_DELAY);
+	}
+}
+
+
+
+bool Mag_Init(void)
+{
+	//
+}
+
+void Mag_Task(void *argument)
+{
+	MagCom_t output = {0};
+
+	for (;;)
+	{
+		xQueueSend(MagQueue, &output, HAL_MAX_DELAY);
+	}
+}
+
+
+
+void Bar30_Init(void)
+{
+	//
+}
+
+void Bar30_Task(void* argument)
+{
+	Bar30Com_t output = {0};
+
+	for (;;)
+	{
+		xQueueSend(Bar30Queue, &output, HAL_MAX_DELAY);
+	}
+}
+
+
+
+bool Humid_Init(void)
+{
+	//
+}
+
+void Humid_Task(void *argument)
+{
+	HumidCom_t output = {0};
+
+	for (;;)
+	{
+		xQueueSend(HumidQueue, &output, HAL_MAX_DELAY);
+	}
+}
+
+
+
+bool Board_Init(void)
+{
+	//
+}
+
+void Board_Task(void *argument)
+{
+	BoardCom_t output = {0};
+
+	for (;;)
+	{
+		xQueueSend(BoardQueue, &output, HAL_MAX_DELAY);
+	}
+}
+
+
+
+uint16_t angle_to_pulse(int16_t angle)
+{
+	//
+}
+
+void Servo_Init(TIM_HandleTypeDef* tim, uint32_t channel)
+{
+	//
+}
+
+void Servo_Task(void* argument)
+{
+	ServoParams_t* params = (ServoParams_t*)argument;
+	ServoCmd_t input;
+	int8_t servo_index = 0;
+
+	// figure out servo index from params
+	if (params->tim == &htim2) servo_index = 0;
+	else servo_index = 4;
+
+	if (params->channel == TIM_CHANNEL_1) servo_index += 0;
+	else if (params->channel == TIM_CHANNEL_2) servo_index += 1;
+	else if (params->channel == TIM_CHANNEL_3) servo_index += 2;
+	else servo_index += 3;
+
+	for (;;)
+	{
+		xQueueReceive(ServoQueue[servo_index], &input, 0);
+	}
+}
+
+
+
+uint16_t percent_to_pulse(int8_t percent)
+{
+	//
+}
+
+void Thruster_Init(void)
+{
+	//
+}
+
+void Thruster_Task(void* arugment)
+{
+	ThrusterCmd_t input;
+
+	for (;;)
+	{
+		xQueueReceive(ThrusterQueue, &input, 0);
+	}
+}
