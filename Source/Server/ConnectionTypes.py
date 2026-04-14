a563279069107200d278b5c438cf8946cbcdf99c
Owen Allison <owal7428@colorado.edu>
2026-04-14 12:13:53 -0600

    Reorganize server code

---
+++ Source/Server/ConnectionTypes.py
@@ -0,0 +1,80 @@
+import ctypes
+
+PACKETSIZE = 128
+
+def CalculateChecksum(packet_bytes):
+    cs = 0
+    for b in packet_bytes[:-1]:
+        cs ^= b
+    return cs
+
+class ServoCmd_t(ctypes.Structure):
+    _fields_ = [
+        ("amplitude", ctypes.c_float),
+        ("speed", ctypes.c_uint16),
+        ("horz_offset", ctypes.c_int16),
+        ("vert_offset", ctypes.c_int16),
+    ]
+
+class ThrusterCmd_t(ctypes.Structure): 
+    _fields_ = [
+        ("thrust", ctypes.c_int8),
+    ]
+
+class IMUCom_t(ctypes.Structure): 
+    _fields_ = [
+        ("acc_x", ctypes.c_float),
+        ("acc_y", ctypes.c_float),
+        ("acc_z", ctypes.c_float),
+        ("gyro_x", ctypes.c_float),
+        ("gyro_y", ctypes.c_float),
+        ("gyro_z", ctypes.c_float),
+    ]
+
+class MagCom_t(ctypes.Structure):
+    _fields_ = [
+        ("head_x", ctypes.c_float),
+        ("head_y", ctypes.c_float),
+        ("head_z", ctypes.c_float),
+    ]
+
+class Bar30Com_t(ctypes.Structure):
+    _fields_ = [
+        ("pressure", ctypes.c_float),
+        ("water_temp", ctypes.c_float),
+    ]
+
+class HumidCom_t(ctypes.Structure):
+    _fields_ = [
+        ("humidity", ctypes.c_float),
+        ("air_temp", ctypes.c_float),
+    ]
+
+class BoardCom_t(ctypes.Structure):
+    _fields_ = [
+        ("board_temp", ctypes.c_float),
+    ]
+
+class InPacket(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("SoP", ctypes.c_uint8*2),
+        ("pad0", ctypes.c_uint8*2), # mimic STM32 padding
+        ("IMUCom", IMUCom_t),
+        ("MagCom", MagCom_t),
+        ("Bar30Com", Bar30Com_t),
+        ("HumidCom", HumidCom_t),
+        ("BoardCom", BoardCom_t),
+        ("CheckSum", ctypes.c_uint8),
+        ("pad1", ctypes.c_uint8), # bring size to 64 bytes total
+    ]
+
+class OutPacket(ctypes.Structure):
+    _fields_ = [
+        ("SoP", ctypes.c_uint8*2),
+        ("pad0", ctypes.c_uint8*2), # mimic STM32 padding
+        ("ServoCmd", ServoCmd_t*8),
+        ("ThrusterCmd", ThrusterCmd_t),
+        ("CheckSum", ctypes.c_uint8),
+        ("pad1", ctypes.c_uint8),
+    ]
